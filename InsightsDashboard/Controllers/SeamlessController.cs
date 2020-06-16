020c8032cea093c503c1a4978adb21bc37e67554
LucasCarver <carver.lucas@gmail.com>
2020-06-16 09:50:45 -0400

    Changed DAL. RAKE in progress

--- InsightsDashboard/Controllers/SeamlessController.cs
+++ InsightsDashboard/Controllers/SeamlessController.cs
@@ -2,21 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InsightsDashboard.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace InsightsDashboard.Controllers
 {
     public class SeamlessController : Controller
     {
+        private readonly InsightsDbContext _context;
+        private readonly SeamlessDAL _seamlessDAL;
 
-        public SeamlessController()
+        public SeamlessController(IConfiguration configuration, InsightsDbContext context)
         {
-
+            _seamlessDAL = new SeamlessDAL(configuration.GetSection("APIKeys")["SeamlessAPIKey"]);
+            _context = context;
         }
 
         public IActionResult Index()
         {
             
+            return View();
+        }
+
+        public IActionResult Tags()
+        {
+            Rake r = new Rake("SmartStoplist.txt");
+            var masterList = _seamlessDAL.GetMasterList();
+            //var keywordsList = masterList.
+            var keywords = r.Run("text");
             return View();
         }
     }
