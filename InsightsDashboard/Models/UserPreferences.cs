953bd0d25513cf6c7c1d1f63b76159b0e548a296
Kendall <allmand.kendall@gmail.com>
2020-06-17 14:17:11 -0400

    db scaffold

---
+++ InsightsDashboard/Models/UserPreferences.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InsightsDashboard.Models
+{
+    public partial class UserPreferences
+    {
+        public int PrefId { get; set; }
+        public string UserId { get; set; }
+        public string StartUpDescription { get; set; }
+        public string FollowedAlignment { get; set; }
+        public string FollowedCountry { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
