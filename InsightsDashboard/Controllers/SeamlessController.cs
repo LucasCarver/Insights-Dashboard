b7438521a35de1b17d180dbb0e1ec16591721a26
Amanda <amandajbledsoe@gmail.com>
2020-06-17 17:28:59 -0400

    Added CRUD Functions for UserStartUp

--- InsightsDashboard/Controllers/SeamlessController.cs
+++ InsightsDashboard/Controllers/SeamlessController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Transactions;
 using InsightsDashboard.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Microsoft.Extensions.Configuration;
@@ -65,8 +69,72 @@ namespace InsightsDashboard.Controllers
             return View(sendit);
         }
 
+        [Authorize]
+        [HttpGet]
+        public IActionResult AddUserDefinedStartup()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public IActionResult AddUserDefinedStartup(UserStartup startUp)
+        {
+            startUp.DateAdded = DateTime.Now;
+            if(ModelState.IsValid)
+            {
+                _context.UserStartup.Add(startUp);
+                _context.SaveChanges();
+                return RedirectToAction("SubmissionComplete", startUp);
+            }
+            else
+            {
+                return RedirectToAction("AddUserDefinedStartup");
+            }
+       }
 
+        public IActionResult SubmissionComplete(UserStartup startUp)
+        {
+            return View(startUp);
+        }
+
+        [Authorize]
+        public IActionResult UserList()
+        {
+            List<UserStartup> userStartUps = new List<UserStartup>();
+            string uId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            userStartUps = _context.UserStartup.Where(x => x.UserId == uId).ToList();
+            return View(userStartUps);
+        }
+
+        [Authorize]
+        public IActionResult RemoveUserStartUp(int id)
+        {
+            UserStartup removeStartUp = _context.UserStartup.Find(id);
+            _context.UserStartup.Remove(removeStartUp);
+            _context.SaveChanges();
+            return RedirectToAction("UserList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateUserStartUp(int id)
+        {
+            UserStartup editStartUp = _context.UserStartup.Find(id);
+            return View(editStartUp);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateUserStartUp(UserStartup newStartUp)
+        {
+            UserStartup startUp = _context.UserStartup.Find(newStartUp.Id);
+            if(ModelState.IsValid)
+            {
+                startUp.CompanyName = newStartUp.CompanyName;
+                _context.Entry(startUp).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _context.Update(startUp);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("UserList");
+        }
 
 
         public async Task<IActionResult> Tags()
