dae3ee279aaac66e9902da3f8d93f86d9ce5cfde
Kendall <allmand.kendall@gmail.com>
2020-06-25 09:25:39 -0400

    tech links

--- InsightsDashboard/Controllers/SeamlessController.cs
+++ InsightsDashboard/Controllers/SeamlessController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Security.Claims;
@@ -31,6 +30,7 @@ namespace InsightsDashboard.Controllers
 
         public IActionResult Index()
         {
+            
             return View();
         }
 
@@ -236,6 +236,116 @@ namespace InsightsDashboard.Controllers
 
 
 
+
+        public async Task<IActionResult> TechDetails(string Tech)
+        {
+
+            TempData["Tech"] = Tech;
+
+
+            Dictionary<string, MainEntry> seamlessDictionary = await _seamlessDAL.GetMainDictionary();
+            Dictionary<string, MainEntry> techDictionary = new Dictionary<string, MainEntry>();
+            string[] techs = new string[] { };
+            int i = 0;
+            foreach (KeyValuePair<string, MainEntry> x in seamlessDictionary)
+            {
+
+                try
+                {
+                    if (x.Value.Themes != null)
+                    {
+                        i++;
+                        string y = x.Value.Themes;
+                        techs = y.Split(',');
+                        x.Value.Themes = "";
+                        foreach (string s in techs)
+                        {
+
+                            x.Value.Themes += s.Trim() + " ";
+                        }
+                    }
+                }
+                catch (IndexOutOfRangeException) { }
+
+
+
+                if (x.Value.Themes != null)
+                {
+                    if (x.Value.Themes.Contains(Tech))
+                    {
+                        techDictionary.Add(x.Key, x.Value);
+                    }
+                }
+
+                try
+                {
+                    if (x.Value.Landscape != null)
+                    {
+                        i++;
+                        string y = x.Value.Landscape;
+                        techs = y.Split(',');
+                        x.Value.Landscape = "";
+                        foreach (string s in techs)
+                        {
+
+                            x.Value.Landscape += s.Trim() + " ";
+                        }
+                    }
+                }
+                catch (IndexOutOfRangeException) { }
+
+
+
+                if (x.Value.Landscape != null)
+                {
+                    if (x.Value.Landscape.Contains(Tech))
+                    {
+                        techDictionary.Add(x.Key, x.Value);
+                    }
+                }
+
+                try
+                {
+                    if (x.Value.TechnologyAreas != null)
+                    {
+                        i++;
+                        string y = x.Value.TechnologyAreas;
+                        techs = y.Split(',');
+                        x.Value.TechnologyAreas = "";
+                        foreach (string s in techs)
+                        {
+
+                            x.Value.TechnologyAreas += s.Trim() + " ";
+                        }
+                    }
+                }
+                catch (IndexOutOfRangeException) { }
+
+
+
+                if (x.Value.TechnologyAreas != null)
+                {
+                    if (x.Value.TechnologyAreas.Contains(Tech))
+                    {
+                        techDictionary.Add(x.Key, x.Value);
+                    }
+                }
+
+            }
+
+            if (TempData["Tech"] != null)
+            {
+                ViewBag.Tech = TempData["Tech"].ToString();
+                return View(techDictionary);
+            }
+
+
+            return View(techDictionary);
+        }
+
+
+
+
         public async Task<IActionResult> AlignmentDetails(string alignment)
         {
 
@@ -294,23 +404,23 @@ namespace InsightsDashboard.Controllers
             string[] alignments = new string[] { };
             int i = 0;
             foreach (KeyValuePair<string, MainEntry> x in seamlessDictionary)
-            {
-                try
-                {
+            {           
+                    try
+                    {
                     if (x.Value.Alignment != null)
                     {
                         i++;
                         string y = x.Value.Alignment;
-                        alignments = y.Split(',');
+                        alignments = y.Split(',');                 
                         x.Value.Alignment = "";
-                        foreach (string s in alignments)
+                        foreach(string s in alignments)
                         {
 
                             x.Value.Alignment += s.Trim() + " ";
                         }
                     }
-                }
-                catch (IndexOutOfRangeException) { }
+                    }
+                    catch (IndexOutOfRangeException) { }
             }
             if(TempData["Status"] != null)
             {
@@ -406,7 +516,8 @@ namespace InsightsDashboard.Controllers
             }
         }
 
-        public async Task<IActionResult> Keywords()
+
+        public async Task<IActionResult> Tags()
         {
             bool stop = false;
             int i = 0;
@@ -428,23 +539,8 @@ namespace InsightsDashboard.Controllers
             }
             // CHARS TO REMOVE
             char[] invalidChars = " !@#$%^&*()_+“”~{}|:\"<>?`1234567890-=[]\\;',./".ToCharArray();
-            List<string> allWords = inputWords.Split(invalidChars).ToList();
+            string[] allWords = inputWords.Split(invalidChars);
             Dictionary<string, int> wordFreq = new Dictionary<string, int>();
-            // REMOVE COMMON WORDS FROM SMARTSTOPLIST.TXT
-            StreamReader streamReader = new StreamReader("wwwroot\\SmartStoplist.txt");
-            List<string> stopList = new List<string>();
-            // DISCARD THE FIRST LINE
-            streamReader.ReadLine();
-            string line;
-            while ((line = streamReader.ReadLine()) != null)
-            {
-                stopList.Add(line.Trim().ToLower());
-            }
-            streamReader.Close();
-            foreach (string stopWord in stopList)
-            {
-                while (allWords.Remove(stopWord)) { }
-            }
 
             // ADD KEYWORDS AND CALCULATE KEYWORD FREQUENCY
             foreach (string word in allWords)
@@ -464,44 +560,6 @@ namespace InsightsDashboard.Controllers
             // ORDER KEYWORDS BY FREQUENCY
             List<KeyValuePair<string, int>> keywordList = wordFreq.OrderByDescending(key => key.Value).ToList<KeyValuePair<string, int>>();
             return View(keywordList);
-        }
-
-        public async Task<IActionResult> KeywordDetails(string keyword, int frequency)
-        {
-            KeyValuePair<string, int> keywordDetails = new KeyValuePair<string, int>(keyword, frequency);
-            // FIND ALL STARTUP OCCURENCES OF THAT WORD
-            Dictionary<string, MainEntry> occurrences = await FindKeywordOccurrences(keyword);
-
-            KeywordDetailsAndOccurrences viewModel = new KeywordDetailsAndOccurrences()
-            {
-                KeywordDetails = keywordDetails,
-                Occurrences = occurrences
-            };
-            return View(viewModel);
-        }
-
-        public async Task<Dictionary<string, MainEntry>> FindKeywordOccurrences(string keyword)
-        {
-            Dictionary<string, MainEntry> mainDictionary = await _seamlessDAL.GetMainDictionary();
-            List<string> twoLineWords;
-            Dictionary<string, MainEntry> subDictionary = new Dictionary<string, MainEntry>();
-            char[] invalidChars = " !@#$%^&*()_+“”~{}|:\"<>?`1234567890-=[]\\;',./".ToCharArray();
-            foreach (var kvp in mainDictionary)
-            {
-                twoLineWords = kvp.Value.TwoLineCompanySummary.ToLower().Split(invalidChars).ToList();
-                foreach (string word in twoLineWords)
-                {
-                    try
-                    {
-                        if (word == keyword)
-                        {
-                            subDictionary.Add(kvp.Key, kvp.Value);
-                        }
-                    }
-                    catch (ArgumentException) { }
-                }
-            }
-            return subDictionary;
         }
     }
 }
